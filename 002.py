c50185a4fd4005a43b7af7b20eddbd32a85794ca
Tim Payne Jr <cabbagehead514@gmail.com>
2019-03-14 15:01:37 -0500

    Added solution to Problem 2

---
+++ 002.py
@@ -0,0 +1,22 @@
+# Solution to Project Euler Problem 1
+# Code by Tim Payne
+# Completed: 03/14/2019
+
+a = 1
+
+b = 2
+
+solution = 2
+
+while b <= 4_000_000:
+
+    a , b = b , a + b
+
+    if b % 2 == 0:
+
+        solution += b
+
+
+print(solution)
+
+    

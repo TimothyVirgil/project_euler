21da03dbbc3db6afcb8c386073bbabd36348c19e
Tim Payne Jr <cabbagehead514@gmail.com>
2019-03-14 14:55:30 -0500

    Added the solution to Problem 1

---
+++ 001.py
@@ -0,0 +1,20 @@
+# Solution to Project Euler Problem 1
+# Code by Tim Payne
+# Completed: 
+
+solution = 0
+
+for a in range(1000):
+
+	if a % 3 == 0 or a % 5 == 0:
+
+		solution += a
+
+print(solution)
+
+
+
+
+
+
+

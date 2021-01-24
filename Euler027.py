5bbed5c71701d820ba8275ff94391e0804c39977
Timothy Payne <timothypaynemath@gmail.com>
2021-01-24 13:50:54 -0500

    Finished # 27

--- Euler027.py
+++ Euler027.py
@@ -1,7 +1,7 @@
 '''Solution to Project Euler Problem 27
 Code by Timothy Virgil Payne Jr.
 Started: 5/13/2020
-Completed:
+Completed: 1/24/2021
 b has to be a prime less than 1000.
 a can be a positive or negative less than 1000. It seems like it will be a
 negative but I am not sure.
@@ -13,58 +13,49 @@ I was trying to find the maximum number of primes and not the maximum consecutiv
 I'll try this again.
 
 '''
-from Euler007 import primecheck
+from Euler007 import primecheck # does what is says
 import time
 start_time = time.time()
 
-max_length = 1
-a_b_prod = 0
+def quad_form(n):
 
-for b in range(3,1000,2):    
-    
-    if primecheck(b):        
-                
-        for a in range(1,1000,2):
-            
-            curr_pos_length = 1
-            
-            for n in range(1,a):              
-                                
-                if primecheck(n**2 + a*n + b):
-                    
-                    curr_pos_length += 1                
-                                   
-                else:
-                    
-                    if curr_pos_length > max_length:
-                        
-                        max_length = curr_pos_length
-                        
-                        a_b_prod = (a * b, a, b, max_length, curr_pos_length)
-                        
-                    break
-                    
-        for a in range(1,1000,2):
-            
-            curr_neg_length = 1
-            
-            for n in range(1,a):              
-                                
-                if primecheck(n**2 - a*n + b):
-                    
-                    curr_neg_length += 1                
-                                   
-                else:
-                    
-                    if curr_neg_length > max_length:
-                        
-                        max_length = curr_neg_length
-                        
-                        a_b_prod = (-a * b, -a, b, max_length, curr_neg_length)
-                        
-                    break
-                    
-print(a_b_prod)
+    global a
+    global b
+
+    output = n**2 + a*n + b
+
+    return output
+
+#need to check for a +- 1 - 1000 and b +- 1 - 1000
+
+# if i do all of them that's 2000 * 2000 which is 4,000,000 checks. Not so many for brute force.
+
+#cycle through all values
+#check for consecutive primes
+#store the a and b for the most consecutive
+#at the end of the day, return a*b
+
+max_prime_tup = (0,0,0)
+
+for a in range(-999,999):
+
+    for b in range(-1000,1001):
+
+        n = 0
+
+        while primecheck(quad_form(n)):
+
+            n += 1
+
+        else:
+
+            if n > max_prime_tup[0]:
+
+                max_prime_tup = (n,a,b)
+
+print(f'n^2 + {max_prime_tup[1]}n + {max_prime_tup[2]} produces {max_prime_tup[0]} consecutive primes.')
+print(max_prime_tup[1]*max_prime_tup[2])
+
                     
 print('Total time: {}'.format(time.time()-start_time))                  
         

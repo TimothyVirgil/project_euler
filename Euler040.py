56cf66e255f4416c6750d18b20167354c17fc93f
Timothy Payne <timothypaynemath@gmail.com>
2021-09-01 10:43:12 -0400

    cleaned up #40

--- Euler040.py
+++ Euler040.py
@@ -7,9 +7,7 @@ Completed: 9/1/21
 num = ''
 
 for a in range(1, 185186):
-
     num = num+str(a)
-
 
 print(int(num[0])*int(num[9])*int(num[99])*int(num[999])*int(num[9999])
       * int(num[99999])*int(num[999999]))

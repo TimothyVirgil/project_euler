b220dfc6c5bee7e4c549784d4f8cbfa6c4110285
Timothy Payne <timothypaynemath@gmail.com>
2022-01-15 17:02:38 -0500

    45 cleanup

--- euler045.py
+++ euler045.py
@@ -5,43 +5,33 @@ Completed: 1/15/22
 '''
 
 def pentest(p):
-    n = int(((1 + 24*p) ** 0.5 + 1)/6)   
-
+    n = int(((1 + 24*p) ** 0.5 + 1)/6) 
     if n*(3*n-1)/2 == p:
         return True
-
     else:
         return False
 
 def hextest(h):
-    n = int(((1 + 8*h) ** 0.5 +1)/4)
-    
+    n = int(((1 + 8*h) ** 0.5 +1)/4)    
     if n*(2*n-1) == h:
-        return True
-    
+        return True    
     else:
         return False
 
-def tripletest(a):
-    
-    tri = a*(a + 1)//2
-    
+def tripletest(a):    
+    tri = a*(a + 1)//2    
     if pentest(tri) and hextest(tri):
-        return tri
-    
+        return tri    
     else:
         return False
 
 def run_test(start=286):
-    x = start
-    
+    x = start    
     while True:    
-        curr_tri = tripletest(x)
-    
+        curr_tri = tripletest(x)    
         if curr_tri != False:
             print(curr_tri)
-            break
-    
+            break    
         else:
             x += 1
             continue

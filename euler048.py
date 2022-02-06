e3cf57904e690a98ec0b4c8d8d9e87901fff9f8d
Timothy Payne <timothypaynemath@gmail.com>
2022-02-06 15:45:55 -0500

    Add #48

---
+++ euler048.py
@@ -0,0 +1,14 @@
+'''Solution to Project Euler Problem 47
+Code by Timothy Virgil Payne Jr.
+Started: 2/6/22
+Completed: 2/6/22
+'''
+
+a=0
+
+for b in range(1,1001):
+    a += b**b
+
+ser_sum=str(a)
+
+print(ser_sum[-10:])

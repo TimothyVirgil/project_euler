b837d7beae0b85ad8514a62d989ec46485515346
Timothy Payne <timothypaynemath@gmail.com>
2023-04-14 15:10:21 -0400

    solve #79

---
+++ 051_100/euler079.py
@@ -0,0 +1,10 @@
+'''Solution to Project Euler Problem 79
+Code by Timothy Virgil Payne Jr.
+Started: 12/22/22
+Completed: 4/14/23
+https://projecteuler.net/problem=79
+
+This file is here for completeness sake.
+I did this one in OneNote.
+Please see euler079.pdf if you are interested in my scratch work.
+'''
